30afb7b20aa3526435a2335c11391f0fb70fba5b
Andrew Bonas <aabonas@hotmail.com>
2020-11-05 11:59:07 -0800

    Flight and Airport models added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Airport.create([
+  {name: 'Vancouver', code: 'YVR'},
+  {name: 'Comox', code: 'YQQ'},
+  {name: 'Kelowna', code: 'YLW'},
+  {name: 'Kamloops', code: 'YKA'},
+  {name: 'Cancelgar', code: 'YCG'},
+  {name: 'Portland', code: 'PDX'},
+])
+
+Flight.create([
+  { departure_airport_id: 1, arrival_airport_id: 2, duration:30, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 1, arrival_airport_id: 3, duration:40, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 1, arrival_airport_id: 4, duration:35, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 1, arrival_airport_id: 5, duration:55, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 1, arrival_airport_id: 6, duration:60, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 1, arrival_airport_id: 2, duration:30, date: 3.months.from_now, time: Time.now},
+
+  { departure_airport_id: 2, arrival_airport_id: 1, duration:40, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 3, arrival_airport_id: 1, duration:50, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 4, arrival_airport_id: 1, duration:45, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 5, arrival_airport_id: 1, duration:60, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 6, arrival_airport_id: 1, duration:60, date: 3.months.from_now, time: Time.now},
+  { departure_airport_id: 2, arrival_airport_id: 1, duration:40, date: 3.months.from_now, time: Time.now},
+])
